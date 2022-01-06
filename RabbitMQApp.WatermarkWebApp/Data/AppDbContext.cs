d785ba4e1c4bd72d0d991a1d9bf352043883b2f6
Yasin Salvator <salvator.yasin@gmail.com>
2022-01-06 15:28:02 +0300

    The database layer of the application has been created

---
+++ RabbitMQApp.WatermarkWebApp/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RabbitMQApp.WatermarkWebApp.Models;
+
+namespace RabbitMQApp.WatermarkWebApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
