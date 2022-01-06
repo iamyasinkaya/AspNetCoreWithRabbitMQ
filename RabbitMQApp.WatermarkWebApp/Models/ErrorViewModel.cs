9173df60e1f762ff939f4815ebbe33f0ac21fcd9
Yasin Salvator <salvator.yasin@gmail.com>
2022-01-06 15:17:15 +0300

    First Commit

---
+++ RabbitMQApp.WatermarkWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RabbitMQApp.WatermarkWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
